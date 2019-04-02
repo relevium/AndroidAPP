06ce2fda1b8f9428cf8ffbb5d1753560098a4bbd
Mohamed-ELTorky <mohamedtarekeltorky@gmail.com>
2019-04-02 19:26:55 +0200

    Changed: LoginActivity now is the starting activity MainActivity no longer has the logic of LoginActivity Tweaked the AndroidManifest file to support the changes

--- app/src/main/java/com/example/android/releviumfinal/MainActivity.java
+++ app/src/main/java/com/example/android/releviumfinal/MainActivity.java
@@ -9,14 +9,6 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Initialize xml intractable variables
-    private Button loginBtn;
-    private Button registerBtn;
-    private Button googleBtn;
-    private Button twitterBtn;
-    private Button facebooknBtn;
-    private EditText userNameEdt;
-    private EditText passwordEdt;
 
 
     @Override
@@ -24,36 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get the resource id from xml file
-        loginBtn = (Button) findViewById(R.id.login);
-        registerBtn = (Button) findViewById(R.id.register);
-        googleBtn = (Button) findViewById(R.id.google);
-        twitterBtn = (Button) findViewById(R.id.twitter);
-        facebooknBtn = (Button) findViewById(R.id.facebook);
-
-        userNameEdt = (EditText) findViewById(R.id.username);
-        passwordEdt = (EditText) findViewById(R.id.password);
-
-        //Set onClick listener for each button
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the Login-in button is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link NumbersActivity}
-                // Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                // Start the new activity
-                //startActivity(numbersIntent);
-            }
-        });
-
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the Login-in button is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link RegisterActivity} and Start the new activity
-                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(registerIntent);
-            }
-        });
     }
 }
--- app/src/main/java/com/example/android/releviumfinal/RegisterActivity.java
+++ app/src/main/java/com/example/android/releviumfinal/RegisterActivity.java
@@ -20,7 +20,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.activity_register);
 
         registerBtn = (Button) findViewById(R.id.submit);
 
