f28b7efc7e02692fb0e492768326044ae2364d45
Mohamed-ELTorky <mohamedtarekeltorky@gmail.com>
2019-04-03 20:05:04 +0200

    Deleted: FragmentPagerAdapter and all the fragment java classes

--- app/src/main/java/com/example/android/releviumfinal/AgentActivity.java
+++ app/src/main/java/com/example/android/releviumfinal/AgentActivity.java
@@ -8,8 +8,6 @@ public class AgentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agent);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new AgentFragment())
-                .commit();
+        
     }
 }
--- app/src/main/java/com/example/android/releviumfinal/MapActivity.java
+++ app/src/main/java/com/example/android/releviumfinal/MapActivity.java
@@ -7,8 +7,6 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new MapFragment())
-                .commit();
+
     }
 }
