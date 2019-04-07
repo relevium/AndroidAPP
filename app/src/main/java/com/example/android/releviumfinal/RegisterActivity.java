67dad733e21fd0e0a65358bc26443f02ff639ab7
Mohamed EL-Torky <mohamedtarekeltorky@gmail.com>
2019-04-07 16:19:01 +0200

    Login and Register are now functional
    Using Fire base to register and handle user's login
    Changed the FAM color to match the action bar

--- app/src/main/java/com/example/android/releviumfinal/RegisterActivity.java
+++ app/src/main/java/com/example/android/releviumfinal/RegisterActivity.java
@@ -1,42 +1,139 @@
 package com.example.android.releviumfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private Button registerBtn;
     private EditText firstnameEdt;
     private EditText lastnameEdt;
-    private EditText usernameEdt;
     private EditText emailEdt;
     private EditText passwordEdt;
     private EditText repasswordEdt;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        FirebaseApp.initializeApp(this);
 
         registerBtn = (Button) findViewById(R.id.submit);
 
         firstnameEdt = (EditText) findViewById(R.id.firstName_register);
         lastnameEdt = (EditText) findViewById(R.id.lastName_register);
-        usernameEdt = (EditText) findViewById(R.id.username_register);
         emailEdt = (EditText) findViewById(R.id.email_register);
         passwordEdt = (EditText) findViewById(R.id.password_register);
         repasswordEdt = (EditText) findViewById(R.id.re_password_register);
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the submit button is clicked on.
-            @Override
-            public void onClick(View view) {
+        mAuth = FirebaseAuth.getInstance();
 
-            }
-        });
+        registerBtn.setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        final String firstName = firstnameEdt.getText().toString().trim();
+        final String lastName = lastnameEdt.getText().toString().trim();
+        final String email = emailEdt.getText().toString().trim();
+        final String password = passwordEdt.getText().toString().trim();
+        final String repassword = repasswordEdt.getText().toString().trim();
+        //final String phone = editTextPhone.getText().toString().trim();
+
+        if (firstName.isEmpty()) {
+            firstnameEdt.setError("first name required");
+            firstnameEdt.requestFocus();
+            return;
+        }
+        if (lastName.isEmpty()) {
+            lastnameEdt.setError("last name required");
+            lastnameEdt.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            emailEdt.setError("email required");
+            emailEdt.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEdt.setError("invalid email address");
+            emailEdt.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            passwordEdt.setError("fill password");
+            passwordEdt.requestFocus();
+            return;
+        }
+
+        if (!password.equals(repassword)) {
+            repasswordEdt.setError("password mismatch");
+            repasswordEdt.requestFocus();
+            return;
+        }
+        //progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            User user = new User(
+                                    firstName,
+                                    lastName,
+                                    email
+                            );
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    //progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                                        Intent loginActivity = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(loginActivity);
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Please try again later", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.submit:
+                registerUser();
+                break;
+        }
     }
 }
